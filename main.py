812a8555cb2164ac97478b1966ea1e2930f9c795
Пользователь <shohjaxondehqonboyev59@gmail.com>
2023-12-12 14:06:03 +0500

    first commit

---
+++ main.py
@@ -0,0 +1,59 @@
+#  # uy ishi birinchi task boshlandi #
+# dictionary = {
+#     "boolen -" : "mantiy qiymat",
+#     "flot -":"o'nlik son",
+#     "for -"  : "biror bir amalni qayta-qayta bajarish sikili",
+#     "if -" : "shartni tekshirish operatori",
+#     "integer -" : "butun son"
+#
+# }
+# for x,y in dictionary.items():
+#     print(x,y)
+#
+# # birinchi uy ishi tugadi #
+
+
+   # 2-uyga vazifa boshlandi #
+# dictionary = {
+#
+#         "aqsh -" : " washington d.c ",
+#         " italiya -" : " rim ",
+#         " malayziya -" : " kuala lumpar ",
+#         " ozbekiston -" : " toshkent ",
+#         " qirgiziston -" : " bishkek ",
+#         " qozoqiston -" : " nur sulton ",
+#         " rossiya -" : " moskova ",
+#         " singapur -" : " sungapur ",
+#         " tojikiston -" : " dushanbe "
+#
+#
+# }
+#
+# for x,y in dictionary.items():
+#     print(f" davlatlari :{x} poytaxtlari:{y}")            # key-valu deb chiqaradi konsolga
+
+#2-uyga vazifa tugadi #
+
+  #3-uyga vazifa boshlandi
+# menu = {
+#     "osh": 25.,
+#     "lag`mon": 15,
+#     "salat": 8.,
+#
+# }
+# taom1 = input("1-taom-")
+# taom2 = input("2-taom-")
+# taom3 = input("3-taom-")
+# if taom1 in menu:
+#     print(f"{taom1}: {menu[taom1]}")
+# else:
+#     print(f"{taom1} menuda mavjud emas!")
+# if taom2 in menu:
+#     print(f"{taom2}: {menu[taom2]}")
+# else:
+#     print(f"{taom2} menuda mavjud emas!")
+# if taom3 in menu:
+#     print(f"{taom3}: {menu[taom3]}")
+# else:
+#     print(f"{taom3} menuda mavjud emas!")
+# 3-uyga vazifa tugadi
